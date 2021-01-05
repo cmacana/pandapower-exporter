50c20dfeb74575eb90da65cde93c6e4b09d4d529
Carlos <carlos.macana@zepben.com>
2021-01-05 20:12:29 +1100

    Reformat style

--- src/ewb-connection.py
+++ src/ewb-connection.py
@@ -2,9 +2,9 @@ import pandas as pd
 import requests
 import pandapower as pp
 
-
 ewb_url = 'http://dev.cppal.ednar.net:9002'
 output_path = ''
+
 
 def get_asset(asset_id):
     asset = {}
@@ -51,7 +51,7 @@ class Feeder:
         assets = {}
         if "feeders" in self.data:
             assets = self.data["feeders"][0]["assets"]
-            pd.DataFrame(assets).to_csv(output_path+'assets.csv')
+            pd.DataFrame(assets).to_csv(output_path + 'assets.csv')
         if "errors" in self.data:
             # displaying the warning message
             message = str(self.data["errors"])
@@ -93,8 +93,8 @@ class Feeder:
 
     def create_pp_buses(self):
         for i, bus in self.buses.iterrows():
-            pp.create_bus(self.net, name=bus["name"], vn_kv=bus["vn_kv"]/1000, busgeodata=bus["busgeodata"])
-        pd.DataFrame(self.net.bus).to_csv(output_path+"pp_buses.csv")
+            pp.create_bus(self.net, name=bus["name"], vn_kv=bus["vn_kv"] / 1000, busgeodata=bus["busgeodata"])
+        pd.DataFrame(self.net.bus).to_csv(output_path + "pp_buses.csv")
         return self.net.bus
 
     def create_pp_lines(self):
@@ -102,15 +102,16 @@ class Feeder:
         for _, conductor in conductors.iterrows():
             from_cn_id = conductor["connections"][0]['connectivityNodeId']
             to_cn_id = conductor["connections"][1]['connectivityNodeId']
-            from_bus = pp.get_element_index(self.net, "bus", from_cn_id )
-            to_bus = pp.get_element_index(self.net, "bus",to_cn_id)
+            from_bus = pp.get_element_index(self.net, "bus", from_cn_id)
+            to_bus = pp.get_element_index(self.net, "bus", to_cn_id)
             length = conductor["length"]
             if length == 0:
                 length = self.min_line_length_km
-            line_name = from_cn_id +"-" + to_cn_id
-            pp.create_line(net=self.net, name=line_name,from_bus=from_bus, to_bus=to_bus, length_km=length, std_type="NAYY 4x50 SE")
-        pd.DataFrame(self.net.line).to_csv(output_path+"pp_lines.csv")
-        conductors.to_csv(output_path+"ewb_conductors.csv")
+            line_name = from_cn_id + "-" + to_cn_id
+            pp.create_line(net=self.net, name=line_name, from_bus=from_bus, to_bus=to_bus, length_km=length,
+                           std_type="NAYY 4x50 SE")
+        pd.DataFrame(self.net.line).to_csv(output_path + "pp_lines.csv")
+        conductors.to_csv(output_path + "ewb_conductors.csv")
         return self.net.line
 
     def create_pp_transformers(self):
@@ -120,13 +121,13 @@ class Feeder:
             hv_bus = pp.get_element_index(self.net, "bus", trafo["connections"][0]['connectivityNodeId'])
             lv_bus = pp.get_element_index(self.net, "bus", trafo["connections"][1]['connectivityNodeId'])
             pp.create_transformer(self.net, hv_bus=hv_bus, lv_bus=lv_bus, std_type="0.4 MVA 20/0.4 kV")
-        pd.DataFrame(self.net.trafo).to_csv(output_path+"pp_trafos.csv")
-        powerTrafos.to_csv(output_path+"ewb_trafos.csv")
+        pd.DataFrame(self.net.trafo).to_csv(output_path + "pp_trafos.csv")
+        powerTrafos.to_csv(output_path + "ewb_trafos.csv")
         return self.net.trafo
 
     def get_connections(self):
         conn = self.assets_df["connections"]
-        conn.to_csv(output_path+"connections.csv")
+        conn.to_csv(output_path + "connections.csv")
         return conn
 
     def create_pp_extgrid(self):
@@ -134,15 +135,16 @@ class Feeder:
         pp.create_ext_grid(net=self.net, bus=head_bus.index.values[0], vm_pu=1, va_degree=0)
         return self.net.ext_grid
 
+
 testFeeder = Feeder(feeder_id="AL002")
-#pp_buses = testFeeder.create_pp_buses()
-#pp_extgrid = testFeeder.create_pp_extgrid()
-#pp_trafos = testFeeder.create_pp_transformers()
-#pp_line = testFeeder.create_pp_lines()
-#pp.create_load(testFeeder.net, bus=0, p_mw=1, q_mvar=0.5,const_z_percent=0,const_i_percent=0,name="Load1")
-#pp.create_load(testFeeder.net, bus=14, p_mw=1, q_mvar=0.5, name="Load2")
-#pp_load = testFeeder.net.load
-#pp.runpp(testFeeder.net)
-#print(testFeeder.net)
-#lf_res = testFeeder.net.res_bus
-#lf_res.to_csv("lf_res_bus")
+# pp_buses = testFeeder.create_pp_buses()
+# pp_extgrid = testFeeder.create_pp_extgrid()
+# pp_trafos = testFeeder.create_pp_transformers()
+# pp_line = testFeeder.create_pp_lines()
+# pp.create_load(testFeeder.net, bus=0, p_mw=1, q_mvar=0.5,const_z_percent=0,const_i_percent=0,name="Load1")
+# pp.create_load(testFeeder.net, bus=14, p_mw=1, q_mvar=0.5, name="Load2")
+# pp_load = testFeeder.net.load
+# pp.runpp(testFeeder.net)
+# print(testFeeder.net)
+# lf_res = testFeeder.net.res_bus
+# lf_res.to_csv("lf_res_bus")
